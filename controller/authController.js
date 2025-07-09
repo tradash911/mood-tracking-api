54f1e32f8cc7fbc5adfc26bfb108e5b69273f17e
tradash@gmail.com <130507016+tradash911@users.noreply.github.com>
2025-07-09 18:16:35 +0200

    i give up

--- controller/authController.js
+++ controller/authController.js
@@ -42,13 +42,24 @@ const signToken = (user) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
-  const cookieOptions = {
+  /*  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     sameSite: "None", // ha külön domain, külön port miatt is!
     secure: true, // production alatt kötelező sameSite: "None" mellé
+  };
+ */
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    sameSite: "Lax", // cross-site cookie
+    secure: false, // csak prodban true
+    path: "/", // mindenhova érvényes
   };
 
   res.cookie("jwt", token, cookieOptions);
