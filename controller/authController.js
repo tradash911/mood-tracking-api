c546c91220a341d1fb1f64d98c4f449ee487ee19
tradash@gmail.com <130507016+tradash911@users.noreply.github.com>
2025-07-09 17:35:57 +0200

    token

--- controller/authController.js
+++ controller/authController.js
@@ -14,7 +14,7 @@ const signToken = (user) => {
   return token;
 };
 
-const createSendToken = (user, statusCode, res) => {
+/* const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
@@ -35,6 +35,29 @@ const createSendToken = (user, statusCode, res) => {
     data: {
       user: user,
     },
+  });
+};
+ */
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    sameSite: "None", // ha külön domain, külön port miatt is!
+    secure: true, // production alatt kötelező sameSite: "None" mellé
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    data: { user },
   });
 };
 
