4427357d6d9f493281e8de5e77b9121377cb9c6c
tradash@gmail.com <130507016+tradash911@users.noreply.github.com>
2025-07-15 09:51:15 +0200

    dsada

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../utils/cloudinary";
+import cloudinary from "../utils/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
