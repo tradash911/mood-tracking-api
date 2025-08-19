3d81b1f38e99ca99de81349b06d54c6bc5209736
tradash@gmail.com <130507016+tradash911@users.noreply.github.com>
2025-08-19 19:18:55 +0200

    dada

--- controller/authController.js
+++ controller/authController.js
@@ -22,11 +22,22 @@ const createSendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     path: "/",
+    sameSite: "none", // <- kisbetű!
+    secure: process.env.NODE_ENV === "production", // mindig együtt a sameSite none-nal
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+  /*  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    path: "/",
     sameSite: "None",
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
-  res.cookie("jwt", token, cookieOptions);
+  res.cookie("jwt", token, cookieOptions); */
 
   ///Remove the password from the output
   user.password = undefined;
