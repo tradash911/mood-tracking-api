076a2847c58e262df4a8db3d5acbcd4a6558f010
tradash@gmail.com <130507016+tradash911@users.noreply.github.com>
2025-08-19 19:23:41 +0200

    dasda

--- controller/authController.js
+++ controller/authController.js
@@ -23,8 +23,9 @@ const createSendToken = (user, statusCode, res) => {
     httpOnly: true,
     path: "/",
     sameSite: "none",
-    secure: process.env.NODE_ENV === "production",
   };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
   res.cookie("jwt", token, cookieOptions);
   /*  const cookieOptions = {
